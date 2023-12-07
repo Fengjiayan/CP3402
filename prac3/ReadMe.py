51b6d8bb158d73dfe5fceaa9c99551cae95b751f
10425 <jiayan.feng@my.jc.edu.au>
2023-12-07 22:16:12 +0800

    complete

---
+++ prac3/ReadMe.py
@@ -0,0 +1,71 @@
+# WordPress Journey: A Week of Discovery and Growth
+
+## Student Information
+- **Name:** Jiayan Feng
+- **Student Number:** 14154574
+
+## Journey Overview
+Through various video platforms such as YouTube, I gained an understanding of the basics of WordPress. This included learning about its history, importance, and general functionality.
+
+- **Starting Point:** The vast possibilities of WordPress initially felt overwhelming. I focused on specific areas to manage this complexity.
+- **Building a WordPress Site:** The practical side involved choosing a hosting service, installing WordPress, and understanding website creation's practicalities.
+- **Customization and Content Creation:** I explored the customization features of WordPress and grappled with the major learning curve in content creation.
+- **SEO Challenges:** Understanding and implementing SEO was the most technically challenging part of this journey.
+
+### Reflection
+At the week's end, I reflected on my progress and the knowledge I had gained. I realized that my WordPress journey is just beginning, with much more to explore, such as advanced customization options, e-commerce integration, and deeper SEO techniques.
+
+### Tutorials
+- `[YouTube Tutorial 1](https://www.youtube.com/watch?v=UT3No6nswz8)`
+- `[YouTube Tutorial 2](https://www.youtube.com/watch?v=O79pJ7qXwoE)`
+
+## Estimated Time
+- Approximately one week.
+
+## Content Insights
+- WordPress demonstrates remarkable flexibility in managing and presenting content. Its ease of use in publishing blogs, managing media, and updating pages makes it ideal for dynamic content creation.
+- Through WordPress, I learned the basics of SEO, realizing its importance for online visibility. Properly optimized content can significantly increase a website’s reach and engagement.
+- The platform also provides deep insights into UX design principles, emphasizing the need for informative and navigable content.
+
+## Careers/Employment/Academic Insights
+- Proficiency in WordPress opens a wide range of career opportunities in digital marketing, content creation, web development, and more.
+- WordPress skills are valuable in the freelance market, especially for creating and managing websites.
+- Engaging with the WordPress community offers professional networking opportunities and insights into the latest digital trends.
+
+## Communication Before Group Work
+Understanding team members' strengths and preferences is crucial for effective collaboration. We conducted interviews to understand each member better:
+
+### Xu Zhanzhi
+- **Skills:** Website content collection and organization.
+- **Team Contribution:** Arranging work, organizing meetings, and task scheduling.
+- **Time Management:** Combining class discussions with after-class study.
+- **Idea Sharing:** Presenting new ideas in group meetings with examples.
+- **Overcoming Challenges:** Searching for information to find solutions to challenges.
+
+### Juan Haoran
+- **Skills:** Effective communication and task coordination.
+- **Time Management:** Prioritizing tasks and using tools like to-do lists and calendars.
+- **Team Contribution:** Bringing expertise and enthusiasm to the team.
+- **Idea Sharing:** Presenting ideas clearly and encouraging open discussions.
+- **Overcoming Challenges:** Addressing challenges through time management and resilience.
+
+### Feng Jiayan
+- **Skills:** Problem-solving and Python coding.
+- **Time Management:** Practicing wise task management and scheduling.
+- **Team Contribution:** Designing database architecture and implementing back-end logic.
+- **Idea Sharing:** Conducting preliminary research before presenting new ideas.
+- **Overcoming Challenges:** Organizing knowledge-sharing sessions for team skill enhancement.
+
+### Yuxuan Liu
+- **Skills:** Web and UI design.
+- **Team Contribution:** Classifying materials and integrating final project elements.
+- **Time Management:** Starting tasks immediately upon receipt.
+- **Idea Sharing:** Utilizing social software for group discussion and idea selection.
+- **Overcoming Challenges:** Believing in sufficient communication to solve problems.
+
+### Zhao Entong
+- **Skills:** Project coordination and communication.
+- **Team Contribution:** Facilitating communication and aligning team goals.
+- **Time Management:** Prioritizing tasks based on urgency and importance.
+- **Idea Sharing:** Evaluating and sharing ideas during meetings.
+- **Overcoming Challenges:** Establishing regular check-ins for workflow consistency.
